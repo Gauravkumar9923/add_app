4b0af74016fd77b6bbe6cb7a2a936891442e012a
Gauravkumar9923 <43923144+Gauravkumar9923@users.noreply.github.com>
2020-04-12 11:26:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.set("view engine", "hbs")
+
+app.use('/pages', require('./routes/pages').route)
+app.use('/api', require('./routes/api').route)
+app.use('/', express.static(path.join(__dirname, 'public_static')))
+
+
+app.listen(8000, () => {
+    console.log("Server started on http://localhost:8000/")
+})
